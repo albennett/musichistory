5752f7e1343a4eece6bcd618c95ef6e04c0f6692
Amy Lee <amyleeannebennett@gmail.com>
2015-12-03 15:29:58 -0600

    got add to work

--- app/controllers/SongFormCtrl.js
+++ app/controllers/SongFormCtrl.js
@@ -3,19 +3,18 @@ app.controller("SongFormCtrl",
     "$scope", 
     "songs-storage", 
     function($scope, songs_storage) {
-      $scope.newSong = { artist: "", album: "", name: ""};
-      $scope.so
+      $scope.newSong = { artist: "", album: "", title: ""};
 
       $scope.addSong = function() {
-        songs_storage.addSong(
-        $scope.songs = songs_storage.addSong($scope.newSong));
+        songs_storage.addSong( $scope.newSong );
+
         songs_storage.loadSongs().then(function(songs){
 
           $scope.song_list = songs;  // Returns all songs
           console.log("$scope.song_list", $scope.song_list);
 
       
-        }) 
+        }); 
       }
     }
   ]
